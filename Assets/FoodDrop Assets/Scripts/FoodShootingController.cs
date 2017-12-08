93bd964ebaa91ca769e591cfd6965a63eeb0549c
Jacek Gulij <jacgulij@gmail.com>
2017-12-08 02:02:03 +0100

    Initial commit

---
+++ Assets/FoodDrop Assets/Scripts/FoodShootingController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodShootingController : MonoBehaviour {
+    
+    public GameObject foodProjectile;
+    public float force;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject PineApple = Instantiate(
+                foodProjectile,
+                transform.position,
+                Random.rotation
+                );
+            Vector3 mouseDirection = Camera.main.ScreenPointToRay(Input.mousePosition).direction;
+            PineApple.GetComponent<Rigidbody>().velocity = mouseDirection * force;
+        }
+    }
+}
---
+++ Assets/Scripts/ChickenShooter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChickenShooter : MonoBehaviour {
+
+	public GameObject meat;
+	public float force;
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			Vector3 position = transform.position;
+			GameObject kurczak = Instantiate (meat, position, Random.rotation);
+			Ray ray = gameObject.GetComponent<Camera> ().ScreenPointToRay (Input.mousePosition);
+			kurczak.GetComponent<Rigidbody> ().velocity = ray.direction * force;
+		}
+	}
+}
