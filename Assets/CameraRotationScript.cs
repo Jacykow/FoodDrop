03779abb1da9d1cc0e9fbd6c07caf591c9b8413e
Jacek Gulij <jacgulij@gmail.com>
2017-12-21 09:09:09 +0100

    Last Prelecture

---
+++ Assets/CameraRotationScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class CameraRotationScript : MonoBehaviour
+{
+    public float speed;
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(new Vector3(0, speed * Time.deltaTime, 0));
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(new Vector3(0, -speed * Time.deltaTime, 0));
+        }
+    }
+}
---
+++ Assets/Scripts/FoodSpawner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class FoodSpawner : MonoBehaviour
+{
+    public GameObject food;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Vector3 pos = transform.position + Vector3.up * 2;
+            Instantiate(food, pos, Random.rotation);
+        }
+    }
+}
