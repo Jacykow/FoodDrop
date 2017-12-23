a904f8fc035a87b25dfd13eaa1e1ec3be775af2e
Jacek Gulij <jacgulij@gmail.com>
2017-12-23 22:21:30 +0100

    Finished

---
+++ Assets/CroissantNumberDisplayController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; //Uwaga nowy import, bo Text 
+
+public class CroissantNumberDisplayController : MonoBehaviour
+{
+    //Do zakodzenia cała klasa z zawartym pomocniczym updatem :)
+
+    private void Update()
+    {
+        string t = "" + 68; //taka sztuczka ze stringami
+        gameObject.GetComponent<Text>().text = t;
+    }
+}
